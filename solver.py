b13abf6e303c8d0d4dd13a9cc2e7270b8f420ca6
Shahin <shahinshahrokh@rocketmail.com>
2020-02-01 10:33:26 +0000

    print_board

--- solver.py
+++ solver.py
@@ -9,3 +9,29 @@ board = [
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
+
+
+def print_board(bo):              # where bo is the board
+    for i in range(len(board)):
+        if i % 3 == 0 and i!=0:   # separate each cell
+            print("---------------------")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+
+
+def main():
+    print_board()
+
+
+
+
+main()
